08ac42599bd1630d04ac6f14736fd69516cdbac1
Rony Varghese <ronyv250289@gmail.com>
2020-09-17 23:29:47 -0400

    Added linting for frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'prettier'
+  ],
+  rules: {
+    'semi': 1,
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'prettier/prettier': [
+      'error', {
+        'semi': true
+      }
+    ]
+  },
+};
